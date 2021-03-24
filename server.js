044ebc8ba6eaaff76d0d16719cc2784c16f9bcb9
PholawatIostream <6206021622071@fitm.kmutnb.ac.th>
2021-03-24 09:08:15 +0700

    update

--- server.js
+++ server.js
@@ -16,4 +16,4 @@ app.use(express.json())
 const subscribersRouter = require('./routes/subscribers')
 app.use('/subscribers', subscribersRouter)
 
-app.listen(6000, () => console.log('Server Started'))
+app.listen(3000, () => console.log('Server Started'))
